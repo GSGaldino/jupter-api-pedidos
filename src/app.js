43a75cc2a63c27e5b6d36792117052b25caa3fcf
Gabriel <gabrielsoaresii@outlook.com>
2020-10-08 15:38:16 -0300

    Enabling cors to frontend

--- src/app.js
+++ src/app.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: 'https://jupter-frontend.vercel.app/',
+  optionsSuccessStatus: 200
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
